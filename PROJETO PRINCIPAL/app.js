9f877495f72b39c7a03fcdb50c5aabd3a74cacdc
Lucas Tepedino <ligeiro.dig@gmail.com>
2026-04-01 11:01:50 -0300

    numero secreto 1

---
+++ PROJETO PRINCIPAL/app.js
@@ -0,0 +1,36 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 1000
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto o chute for diferente do n.s ...
+while (chute != numeroSecreto){
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se o chute for igual ao n.s.
+    if (chute == numeroSecreto) {
+        break;
+        
+    } else {
+        if(chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+            //TEMPLATE STRINGS ${}
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1
+        tentativas++
+    }   
+} // OPERADOR TERNÁRIO
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+//if (tentativas > 1) {
+ //   alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+//} else {
+//    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+//}
+
+
+
