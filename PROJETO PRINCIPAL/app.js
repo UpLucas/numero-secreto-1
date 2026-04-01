5f9555e42e3f4d10c51bb7cb93e703311cfb336d
Lucas Tepedino <ligeiro.dig@gmail.com>
2026-04-01 12:14:32 -0300

    alterando para 2000

--- PROJETO PRINCIPAL/app.js
+++ PROJETO PRINCIPAL/app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroMaximo = 900
+let numeroMaximo = 2000
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
